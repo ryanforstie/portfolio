8f5f307710c980d346ca690eaed5da5facab0cfd
ryanforstie <ryanforstie@gmail.com>
2017-08-18 08:25:36 -0700

    working on new test page

---
+++ test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var portfolioView = {};
+
+portfolioView.handleMainNav = function() {
+
+  $('#main-nav .tab').on('click', function() {
+    var $whereToGo = $(this).attr('data-content');
+    $('.tab-content').hide();
+    $('#' + $whereToGo).fadeIn(1000);
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
+$(document).ready(function() {
+  portfolioView.handleMainNav();
+});
