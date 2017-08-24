1da865741252bd1df3e5c26ac6b661660c0c6d96
ryanforstie <ryanforstie@gmail.com>
2017-08-23 20:57:00 -0700

    got functionality for handlebars to workgit add .

---
+++ public/scripts/project.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var projects = [];
+
+function Project(rawDataObj) {
+  this.title = rawDataObj.title;
+  this.body = rawDataObj.body;
+}
+
+Project.prototype.toHtml = function() {
+  var projectTemplate = $('#project-template').html();
+  var templateRender = Handlebars.compile(projectTemplate);
+
+  return templateRender(this);
+}
+
+rawData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+});
